7b6e0f38f4e05b3b0bb8ef81a49bb92cdd4f1c3b
noelfranthomas <noelfranthomas@gmail.com>
2021-07-01 00:11:11 -0600

    trying lottieAnim

--- src/components/lottieAnims/ImpressionLottie.js
+++ src/components/lottieAnims/ImpressionLottie.js
@@ -1,24 +1,22 @@
 import React from 'react';
+// import '@lottiefiles/lottie-player';
 
 class impressionLottie extends React.Component {
-  componentDidMount() {
-    import("@lottiefiles/lottie-player")
-      .then((lottiePlayer) => {
-        this.lottiePlayer = lottiePlayer;
-      })
-      .catch((error) => console.error(error));
-  }
+  comp
   render() {
     return (
       <div className="App">
-        <lottie-player
+        {/* <lottie-player
           loop
           autoplay
           ref={this.myRef} // 2. set the reference for the player
           id="zeroLottie"
           mode="normal"
           src="https://assets7.lottiefiles.com/packages/lf20_3zlw7rw7.json"
-        ></lottie-player>
+        ></lottie-player> */}
+        <p>
+          LottieAnim would be here..
+        </p>
       </div>
     );
   }
