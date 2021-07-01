ef1404c308d80f4e3f102077e65d1de78fac6359
noelfranthomas <noelfranthomas@gmail.com>
2021-07-01 13:38:32 -0600

    Removed one anim (still importing tho)

--- src/components/lottieAnims/ImpressionLottie.js
+++ src/components/lottieAnims/ImpressionLottie.js
@@ -1,21 +1,19 @@
 import React from 'react';
-import loadable from '@loadable/component'
-import '@lottiefiles/lottie-player'
-
-const LottiePlayer = loadable(() => import('@node/@lottiefiles/lottie-player/'))
+import '@lottiefiles/lottie-player';
 
 class impressionLottie extends React.Component {
   render() {
     return (
       <div className="App">
-        <LottiePlayer
+        {/* <lottie-player
           loop
           autoplay
           ref={this.myRef} // 2. set the reference for the player
           id="zeroLottie"
           mode="normal"
           src="https://assets7.lottiefiles.com/packages/lf20_3zlw7rw7.json"
-        ></LottiePlayer>
+        ></lottie-player> */}
+        <p>LottieAnim Here</p>
       </div>
     );
   }
